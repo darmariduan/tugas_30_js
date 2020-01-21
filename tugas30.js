984697fb5200faf02a7b7c658740dbb9cc3eca32
Darma Riduan <dharmarhiduwhan@gmail.com>
2020-01-21 11:18:37 +0700

    Tugas 30 Js

---
+++ tugas30.js
@@ -0,0 +1,5 @@
+function panggilRegexp(value) {
+  var data = value.match(/[es]/g);
+  console.log(data);
+}
+panggilRegexp('Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat');
